eb2514df5aca901385aa2399ea1fd9b1d2879bc1
applica-ralfonso <ralfonso@applica.dev>
2023-12-20 16:25:01 -0300

    update service worker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,12 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    })]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerImmediately',
+    }),
+  ],
 };
