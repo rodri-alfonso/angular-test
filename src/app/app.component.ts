b2ab41d4463b4135c457fcbbf0eb39cc7aa9716c
applica-ralfonso <ralfonso@applica.dev>
2023-12-21 17:05:36 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SwPush } from '@angular/service-worker';
-import webpush from 'web-push';
 
 @Component({
   selector: 'app-root',
@@ -39,39 +38,6 @@ export class AppComponent {
   }
 
   sendNotification() {
-    const pushSubscription = {
-      endpoint:
-        'https://fcm.googleapis.com/fcm/send/cww-qIUbcDw:APA91bHVzOqVFBPeQyif-5F0BxMRsoQjyw2roXPW21pBPks45mdUD4kYt_8N71qy1lVi0Yo6YcAYGi7xVaWVtmJgoEUpMotrZO1eBZGokdvTItX5rG4hS_sN4dFnvDFLpf7H34-D9MfY',
-      keys: {
-        p256dh:
-          'BHozctADQY4dOmYfkweU3_av1OBL9IJ_NCVxMpuALzkhg_cgZ-oGeTI6_y-nmLXWSQCGIYDn4WfBV0TnDwTjql8',
-        auth: 'MPbZ-Lrwi6WodkEbRsbGFA',
-      },
-    };
-
-    const payload = {
-      notification: {
-        title: '😄😄 Saludos',
-        body: 'Subscribete a mi canal de YOUTUBE',
-        vibrate: [100, 50, 100],
-        // image:
-        // 'https://avatars2.githubusercontent.com/u/15802366?s=460&u=ac6cc646599f2ed6c4699a74b15192a29177f85a&v=4',
-        actions: [
-          {
-            action: 'explore',
-            title: 'Go to the site',
-          },
-        ],
-      },
-    };
-
-    webpush
-      .sendNotification(pushSubscription, JSON.stringify(payload))
-      .then((res: any) => {
-        console.log('Enviado !!', res);
-      })
-      .catch((err: any) => {
-        console.log('Error', err);
-      });
+    console.log('se envio!');
   }
 }
